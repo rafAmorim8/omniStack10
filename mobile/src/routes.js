972578748e1ef941da7c849492fd39b369cebb0a
rafAmorim8 <rafael_amorim30@hotmail.com>
2020-02-06 16:27:06 -0800

    Finishes core mobile application

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -6,8 +6,27 @@ import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
   createStackNavigator({
-    Main,
-    Profile,
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar'
+      },
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Perfil no Github'
+      },
+    },
+  }, {
+    defaultNavigationOptions: {
+      headerTintColor: '#fff',
+      headerBackTitleVisible: false,
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: '#7D40E7',
+      },
+    },
   })
 );
 
